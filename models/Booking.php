4b190f40bed800f6c3349fbceedc0981670172c3
Viktorija Valsoe <johndoe@example.com>
2019-08-23 18:59:35 +0200

    Created fuction read wit hquery to extract our bookings

--- models/Booking.php
+++ models/Booking.php
@@ -5,9 +5,38 @@
     private $conn;
     private $table = 'Booking';
 
-    //Post Properties
+    //Booking Properties
     public $id;
     public $customer_id;
-    public $guest_qty;
+    public $guest_nr;
     public $date;
+
+    //Constructor with DB
+    public function __constructor($db) {
+      $this->conn = $db;
+    }
+
+    //Get Bookings
+    public function read() {
+      //Create query 
+      $query =
+        'SELECT 
+          b.id,
+          b.customer_id,
+          b.guest_nr,
+          b.date
+          FROM
+          ' .$this->table.' b
+          LEFT JOIN
+          Customers c ON b.customer_id = c.id
+          ';
+      
+      //Prepare statement
+      $stmt = $this->conn->prepare($query);
+
+      //Execute statement
+      $stmt->execute();
+
+      return $stmt;
+    }
   }
