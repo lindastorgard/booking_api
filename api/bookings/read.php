c88b5c0463c1f6fc0c57348fc594408c5e313fd3
lindastorgard <linda.storgard@gmail.com>
2019-08-25 20:13:45 +0200

    add http_response_code

--- api/bookings/read.php
+++ api/bookings/read.php
@@ -3,7 +3,7 @@
 //CORS (Cross-Origin Resource Sharing) header
 //Should be changed to http://localhost:3000/
 header('Access-Control-Allow-Origin: *');
-header('Conternt-Type: aplication/json');
+header('Conternt-Type: application/json; ; charset=UTF-8');
 
 include_once '../../config/Database.php';
 include_once '../../models/Booking.php';
@@ -41,10 +41,18 @@ if($num > 0) {
     array_push($booking_arr['data'], $booking_item);
   }
 
+  // set response code - 200 OK
+  http_response_code(200);
+
   //Turn to JSON & output
   echo json_encode($booking_arr);
-  } else {
+  } else {  
+  
+  // set response code - 404 not found
+  http_response_code(404);
+  
   echo json_encode(
+    
     array('message' => 'No Bookings Found')
   );
 }
