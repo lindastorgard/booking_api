464c8df50bf23d59a4bcbd17efaa228cfaef7ee4
lindastorgard <linda.storgard@gmail.com>
2019-08-25 20:17:41 +0200

    change query to output all in Booking Table

--- models/Booking.php
+++ models/Booking.php
@@ -1,7 +1,7 @@
 <?php
   class Booking {
      
-    //Database
+    //Database connection & table name
     private $conn;
     private $table = 'Booking';
 
@@ -12,24 +12,25 @@
     public $date;
 
     //Constructor with DB
-    public function __constructor($db) {
+    public function __construct($db) {
       $this->conn = $db;
     }
 
     //Get Bookings
     public function read() {
-      //Create query 
-      $query =
-        'SELECT 
-          b.id,
-          b.customer_id,
-          b.guest_nr,
-          b.date
-          FROM
-          ' .$this->table.' b
-          LEFT JOIN
-          Customers c ON b.customer_id = c.id
-          ';
+      // Create query 
+      $query = ('SELECT * from ' . $this->table);
+        // 'SELECT 
+        //   id,
+        //   customer_id,
+        //   guest_nr,
+        //   date
+        //   FROM
+        //   ' . $this->table .' 
+          
+        //   LEFT JOIN
+        //   Customers ON customer_id = id
+        //   ';
       
       //Prepare statement
       $stmt = $this->conn->prepare($query);
