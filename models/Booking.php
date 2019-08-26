9082a18f7f27a6028f3d076ad5efdbf5ed4811c0
Viktorija Valsoe <johndoe@example.com>
2019-08-26 17:37:46 +0200

    removed readSingleBooking() because it was not working

--- models/Booking.php
+++ models/Booking.php
@@ -32,33 +32,6 @@
 
       return $stmt;
     }
-
-
-    //GET Single Booking
-    public function readSingleBooking(){
-      $query = (
-        'SELECT * FROM Booking 
-        JOIN Customers ON Booking.customer_id = Customer.id 
-        WHERE Booking.id = ?'
-      );
-
-    //Prepare statement
-      $stmt = $this->conn->prepare($query);
-
-    //BIND ID
-     $stmt->bindParam(1, $this->id);
-
-    //Execute statement
-      $stmt->execute();
-
-    //fetch an associative array
-      $row = $stmt->fetch(PDO::FETCH_ASSOC);
-
-    //Set properties
-      $this->customer_id = $row['customer_id'];
-      $this->guest_nr = $row['guest_nr'];
-      $this->date = $row['date'];    
-      }
     
     // Create Booking
 
