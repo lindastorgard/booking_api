4cb52d6c9c63e3277f791d2b2acf4dc49a365800
micsson <michaelperssonmail@gmail.com>
2019-08-26 13:53:20 +0200

    add cr to api

---
+++ api/bookings/create.php
@@ -0,0 +1,40 @@
+<?php
+//Headers
+//CORS (Cross-Origin Resource Sharing) header
+//Should be changed to http://localhost:3000/
+header('Access-Control-Allow-Origin: *');
+header('Conternt-Type: application/json; ; charset=UTF-8');
+header('Access-Control-Allow-Methods: POST');
+header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type
+Access-Control-Allow-Methods, Authorization, X-Requested-With');
+
+
+include_once '../../config/Database.php';
+include_once '../../models/Booking.php';
+
+//Instantiate DB & connect
+$database = new Database();
+$db = $database->connect();
+
+//Instantiate blog bookings object
+
+$booking = new Booking($db);
+
+// Get raw posted data
+$data = json_decode(file_get_contents("php://input"));
+
+$booking->id = $data->id;
+$booking->customer_id = $data->customer_id;
+$booking->guest_nr = $data->guest_nr;
+$booking->date = $data->date;
+
+//Create booking
+if($post->create()) {
+    echo json_encode(
+        array('message' => 'Post Created')
+    );
+} else {
+    echo json_encode(
+        array('message' => 'Post Not Created')
+    );
+} 
