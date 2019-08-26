650db0db00acb4458a6ec95b2cdc85ed2e9c3951
lindastorgard <linda.storgard@gmail.com>
2019-08-26 21:54:24 +0200

    add delete() to Customer

--- models/Customer.php
+++ models/Customer.php
@@ -32,4 +32,34 @@
 
       return $stmt;
     }
+
+
+    // Delete customer
+    public function delete(){
+      // Create query
+      $query = 
+      'DELETE FROM 
+        Customer 
+      WHERE 
+        id = :id';
+    
+      //Prepare statement
+      $stmt = $this->conn->prepare($query);
+
+      // Clean data
+      $this->id = htmlspecialchars(strip_tags($this->id));
+
+      // Bind data
+      $stmt->bindParam(':id', $this->id);
+
+      //Execute statement
+      if($stmt->execute()){
+        return true;
+      }
+      
+      // Print error if something goes wrong
+      printf('Error: %s.\n', $stmt->error);
+
+      return false;
+    }
   }
