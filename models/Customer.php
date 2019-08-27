462a90c22a79122167482e79be00280e69c3c064
micsson <michaelperssonmail@gmail.com>
2019-08-27 09:52:11 +0200

    add createCustomer function

--- models/Customer.php
+++ models/Customer.php
@@ -17,6 +17,46 @@
       $this->conn = $db;
     }
 
+    public function createCustomer() {
+      //Create query
+      $query = 'INSERT INTO ' .
+          $this->table . '
+        SET
+          id = :id,
+          name = :name,
+          lastname = :lastname,
+          email = :email,
+          phone = :phone';
+
+      // Prepare statement
+      $stmt = $this->conn->prepare($query);
+
+      //Clean data
+      $this->id = htmlspecialchars(strip_tags($this->id));
+      $this->name = htmlspecialchars(strip_tags($this->name));
+      $this->lastname = htmlspecialchars(strip_tags($this->lastname));
+      $this->email = htmlspecialchars(strip_tags($this->email));
+      $this->phone = htmlspecialchars(strip_tags($this->phone));
+
+      // Bind data
+      $stmt->bindParam(':id', $this->id);
+      $stmt->bindParam(':name', $this->name);
+      $stmt->bindParam(':lastname', $this->lastname);
+      $stmt->bindParam(':email', $this->email);
+      $stmt->bindParam(':phone', $this->phone);
+
+      //Execute query
+      if($stmt->execute()) {
+        return true;
+      }
+
+      // Print error if something goes wrong
+      printf('Error: %s.\n', $stmt->error);
+      return false;
+
+    }
+
+
     //Get Customers
     public function readCustomers() {
       // Create query
